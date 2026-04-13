c642dfbf01b10173c3776f1485838ca3fff308a9
jeff980101 <jeff980101@gmail.com>
2026-04-13 13:53:09 +0800

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1YydRzMyjaQT8Od3rno-HZui0OiVhP6zt
 """
 
-pip install requests beautifulsoup4
 
 import requests
 from bs4 import BeautifulSoup
